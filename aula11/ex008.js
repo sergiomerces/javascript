40a41ccd3110c5b8cb53dea48492cacc4f22351c
sergiomerces <sergio.merces@gmail.com>
2021-10-13 15:56:58 -0300

    exe008 e ex009 aplicando condicionais com if

---
+++ aula11/ex008.js
@@ -0,0 +1,11 @@
+var vel = 50.5;
+
+console.log(`A velocidade do seu carro é ${vel} km/h`);
+
+if(vel > 60) {
+    console.log('Você ultrapassou a velocidade permitida. MULTADO!');
+ } else {
+     console.log('Dirija sempre usando cinto de segurança!');
+ }
+
+
