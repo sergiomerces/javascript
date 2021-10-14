0adf7af63b81002230169f4caa8843a89878a966
sergiomerces <sergio.merces@gmail.com>
2021-10-14 16:13:54 -0300

    aula12 estruturas de seleção simples e múltiplas e manipulação de data e hora do sistema

---
+++ aula12/ex011.js
@@ -0,0 +1,9 @@
+var idade = 22;
+
+if(idade < 16) {
+    console.log('Não vota');
+} else if(idade < 18) {
+        console.log('Voto facultativo');
+} else {
+    console.log('Voto obrigatório');
+}
