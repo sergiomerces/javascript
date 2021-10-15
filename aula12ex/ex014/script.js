393d7caab13cfa71284ba41b3153cce021c39987
sergiomerces <sergio.merces@gmail.com>
2021-10-15 11:21:37 -0300

    ex014 hora do dia concluído

--- aula12ex/ex014/script.js
+++ aula12ex/ex014/script.js
@@ -9,15 +9,19 @@ function carregar() {
     if(hora < 6) {
         //Boa madrugada!
         img.src = 'madrugada.png';
+        document.body.style.background = 'rgb(0, 0, 0)';
     } else if(hora < 12) {
         //Bom dia!
         img.src = 'manha.png';
+        document.body.style.background = 'rgb(239, 215, 181)';
     } else if(hora <= 18) {
         //Boa tarde!
         img.src = 'tarde.png';
+        document.body.style.background = 'rgb(119, 151, 102)';
     } else {
         //Boa noite!
         img.src = 'noite.png';
+        document.body.style.background = 'rgb(3, 71, 80)';
     }
 }
 
