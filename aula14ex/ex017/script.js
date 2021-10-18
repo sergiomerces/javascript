99eb725c829b8fd887fad1f166758ddb7cf7d811
sergiomerces <sergio.merces@gmail.com>
2021-10-17 23:52:22 -0300

    ex017 - Tabuada usando contado e concatenação no método .innerHTML

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,1 +1,16 @@
-window.alert('Olá');
+function gerar() {
+    let num = document.querySelector('input#num');
+    let res = document.querySelector('div#res');
+
+    if (num.value.length === 0) {
+        window.alert('Digite um número válido para que possa ser exibida a tabuada!');
+    } else {
+        num = Number(num.value);
+
+        for(let mult = 0; mult <= 10; mult++) {
+            let pro = num * mult;
+            res.innerHTML += `${num} X ${mult} = ${pro}<br>`;
+        }
+        res.innerHTML += '<br>Fim!<br>';
+    }
+}
