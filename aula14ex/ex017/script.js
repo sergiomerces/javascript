d5c907995ac3ad6225c367a50f40dbb36b7858e6
sergiomerces <sergio.merces@gmail.com>
2021-10-18 03:45:02 -0300

    ex17 - Tabuada colocand resulados dentro de um select com itens dinâmicos

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,16 +1,27 @@
 function gerar() {
     let num = document.querySelector('input#num');
-    let res = document.querySelector('div#res');
+    let tab = document.querySelector('select#seltab');
+    //let res = document.querySelector('div#res');
 
     if (num.value.length === 0) {
         window.alert('Digite um número válido para que possa ser exibida a tabuada!');
     } else {
+        //para limpar o select caso já exista algo
+        tab.innerHTML = null;
+
         num = Number(num.value);
 
         for(let mult = 0; mult <= 10; mult++) {
             let pro = num * mult;
-            res.innerHTML += `${num} X ${mult} = ${pro}<br>`;
+            let  item = document.createElement('option');
+            
+            item.text = `${num} X ${mult} = ${pro}`;
+            //vai gerar o value tab1 tab2 tab3 para o controle php
+            item.value = `tab${mult}`;
+            
+            tab.appendChild(item);
+            //res.innerHTML += `${num} X ${mult} = ${pro}<br>`;
         }
-        res.innerHTML += '<br>Fim!<br>';
+        //res.innerHTML += '<br>Fim!<br>';
     }
 }
