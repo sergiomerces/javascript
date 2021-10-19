9970861409082e50a8fd123c6f7a68a56d0e47d4
sergiomerces <sergio.merces@gmail.com>
2021-10-18 23:38:49 -0300

    aula15 - introdução aos vetores e manipulação de seus elementos

---
+++ aula15/array.js
@@ -0,0 +1,15 @@
+let num = [5, 8, 4];
+num[3] = 6;
+
+console.log(num);
+console.log(`${num}`);
+
+console.log(num.length);
+
+num.sort();
+console.log(num);
+console.log(num[1]);
+
+for(let i = 0; i <= num.length; i++) {
+    console.log(num[i]);
+}
---
+++ aula15/array2.js
@@ -0,0 +1,9 @@
+let num = [4, 5, 6, 7, 8];
+
+let pos = num.indexOf(9);
+
+if (pos !== -1) {
+    console.log(`O número procurado está na  ${pos} do Array.`);
+} else {
+    console.log('O número procurado não está no Array!');
+}
