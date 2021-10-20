fdf3d8a3ba3bdf1e71508e637571123593fb2ed0
sergiomerces <sergio.merces@gmail.com>
2021-10-19 23:56:21 -0300

    aula16 - classifcação e exibição total, maior, menor, soma e média dos valores da lista

--- aula16ex/analisador/script.js
+++ aula16ex/analisador/script.js
@@ -27,9 +27,42 @@ function adicionar() {
 
         item.text = `valor ${num.value} adicionado`;
         caixa.appendChild(item);
+        res.innerHTML = null;
     } else {
         window.alert('Valor inválido ou já encontrado na lista!');
     }
     num.value = null;
     num.focus();
+}
+
+function finalizar() {
+    if (valores.length === 0) {
+        window.alert('Digite um valor para adicionar à lista!');
+    } else {
+        let total = valores.length;
+        let maior = valores[0];
+        let menor = valores[0];
+        let soma = null;
+        let media = null;
+
+        for (let pos in valores) {
+            if (valores[pos] > maior) {
+                maior = valores[pos];
+            }
+
+            if (valores[pos] < menor) {
+                menor = valores[pos];
+            }
+
+            soma += valores[pos];
+            media = soma / total;
+        }
+
+        res.innerHTML = null;
+        res.innerHTML += `<p>Ao <strong>total</strong> temos <strong>${total}</strong> números lançados.</p>`;
+        res.innerHTML += `<p>O <strong>maior</strong> valor informado foi <strong>${maior}</strong>.</p>`;
+        res.innerHTML += `<p>O <strong>menor</strong> valor informado foi <strong>${menor}</strong>.</p>`;
+        res.innerHTML += `<p>A <strong>soma</strong> de todos os valores é <strong>${soma}</strong>.</p>`;
+        res.innerHTML += `<p>A <strong>média</strong> dos valores digitados é <strong>${media}</strong>.</p>`;
+    }
 }
